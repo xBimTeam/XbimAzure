df8dad48130bfc5e6ec84cdfdf029fe6436e0f33
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-02-20 17:16:40 +0100

    Toolbar development started.

--- XbimModelPortal/Scripts/main.js
+++ XbimModelPortal/Scripts/main.js
@@ -1,26 +1,26 @@
 ﻿$(document).ready(function () {
-    var queryString = function () {
-        // This function is anonymous, is executed immediately and 
-        // the return value is assigned to QueryString!
-        var query_string = {};
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i = 0; i < vars.length; i++) {
-            var pair = vars[i].split("=");
-            // If first entry with this name
-            if (typeof query_string[pair[0]] === "undefined") {
-                query_string[pair[0]] = pair[1];
-                // If second entry with this name
-            } else if (typeof query_string[pair[0]] === "string") {
-                var arr = [query_string[pair[0]], pair[1]];
-                query_string[pair[0]] = arr;
-                // If third or later entry with this name
-            } else {
-                query_string[pair[0]].push(pair[1]);
-            }
-        }
-        return query_string;
-    }();
+    //var queryString = function () {
+    //    // This function is anonymous, is executed immediately and 
+    //    // the return value is assigned to QueryString!
+    //    var query_string = {};
+    //    var query = window.location.search.substring(1);
+    //    var vars = query.split("&");
+    //    for (var i = 0; i < vars.length; i++) {
+    //        var pair = vars[i].split("=");
+    //        // If first entry with this name
+    //        if (typeof query_string[pair[0]] === "undefined") {
+    //            query_string[pair[0]] = pair[1];
+    //            // If second entry with this name
+    //        } else if (typeof query_string[pair[0]] === "string") {
+    //            var arr = [query_string[pair[0]], pair[1]];
+    //            query_string[pair[0]] = arr;
+    //            // If third or later entry with this name
+    //        } else {
+    //            query_string[pair[0]].push(pair[1]);
+    //        }
+    //    }
+    //    return query_string;
+    //}();
 
     function initControls() {
 
@@ -320,10 +320,6 @@
        
 
     });
-    //requirements browser loads static data for now
-    //rBrowser.load('/Services/GetData?model=LakesideRestaurantSpec.json');
-
-
 
 
     //viewer set up
@@ -365,6 +361,15 @@
         }
     }
 
+    // ---------------------------------- FOR DEVELOPMENT ONLY ------------------------------------ //
+    if (true) { //set this to false for production
+        //Hide upload overlay
+        $("#overlay").hide(200);
 
+        //Load default data    
+        browser.load("Data/LakesideRestaurant.json");
+        viewer.load("Data/LakesideRestaurant.wexbim");
+    }
+    // ----------------------------- END OF DEVELOPMENT SECTION ----------------------------------- //
 
 });
