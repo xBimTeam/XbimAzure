a841c01fd9d963803e63c6e85aaa9e0b13862b8d
Martin Cerny <martin.cerny@northumbria.ac.uk>
2015-04-16 17:05:22 +0200

    Updated NuGet packages, preparation for handover - simple pages to do the tasks.

---
+++ XbimModelPortal/Controllers/ComplianceController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XbimModelPortal.Controllers
+{
+    public class ComplianceController : CloudStorageController
+    {
+        // GET: Compliance
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Convert()
+        {
+            return View();
+        }
+
+        public ActionResult Review()
+        {
+            return RedirectToAction("Index","Model");
+        }
+
+        public ActionResult Validate()
+        {
+            return View();
+        }
+
+        public ActionResult Verify()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ XbimModelPortal/Scripts/stepbystep.functions.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function() {
+    $(".input-group .file-button").click(function() {
+        $(this).parent().find("input[type='file']").click();
+    });
+    $(".input-group input[type='file']").change(function() {
+    	var file = this.files[0];
+    	if (file) {
+    		$(this).parent().find("input[type='text']").val(file.name);
+	    }
+    });
+});
+
+function whenReady(controller, model, callback) {
+// ReSharper disable once InconsistentNaming
+	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.open("GET", controller + "/IsModelReady?model=" + model, true);
+	xmlhttp.onreadystatechange = function () {
+		if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+			var data = JSON.parse(xmlhttp.responseText);
+			if (data.State === "READY")
+				callback(model);
+			else
+				setTimeout(function () { whenReady(model, callback) }, 500);
+		}
+	};
+	xmlhttp.send();
+}
+
+function reportProgress(message) {
+    $("#progress-report").text(message);
+}
